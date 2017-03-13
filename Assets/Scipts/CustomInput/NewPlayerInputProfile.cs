24c2e8d4732767e55e127251144f0bb9819a07b5
Jip Sterk <jipsterk1998@gmail.com>
2017-03-13 14:45:56 +0100

    Imported DOTween

---
+++ Assets/Scipts/CustomInput/NewPlayerInputProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Brawler.CustomInput
+{
+    public static class NewPlayerInputProfile
+    {
+    }
+}
---
+++ Assets/Scipts/UI/InGameHud.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Brawler.GameManagement;
+using Brawler.GameSettings;
+using Brawler.Items;
+using Brawler.LevelManagment;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Brawler.UI
+{
+    public class InGameHud : MonoBehaviour
+    {
+        [SerializeField] private Text _text;
+        [SerializeField] private Slider _p1HealthBar;
+        [SerializeField] private Slider _p2HealthBar;
+        
+        private GameManager _gameManager;
+        private float _gameTime;
+
+        private void Start()
+        {
+            _gameManager = GameManager.Instance;
+            _gameManager.OnMatchStart += Init;
+        }
+
+        private void Init(GamePlayer p1GamePlayer, GamePlayer p2GamePlayer, List<Item> items, Level level, MatchSettings matchSettings)
+        {
+            _gameTime = matchSettings.GameTime;
+            _text.text = _gameTime.ToString(CultureInfo.InvariantCulture);
+
+            p1GamePlayer.Character.OnCharacterDamage += UpdateP1;
+            p2GamePlayer.Character.OnCharacterDamage += UpdateP2;
+
+            _p1HealthBar.value = p1GamePlayer.Character.CharacterStats.Health;
+            _p2HealthBar.value = p2GamePlayer.Character.CharacterStats.Health;
+        }
+        
+        private void Update()
+        {
+            if(_gameManager.MenuState != MenuState.OfflineMultiplayer) 
+                return;
+
+            _gameTime -= Time.deltaTime;
+            _text.text = _gameTime.ToString("F0");
+        }
+
+        private void UpdateP1(float health)
+        {
+            _p1HealthBar.value = Mathf.Lerp(_p1HealthBar.value, health, Time.deltaTime);
+        }
+
+        private void UpdateP2(float health)
+        {
+            _p2HealthBar.value = Mathf.Lerp(_p2HealthBar.value, health, Time.deltaTime);
+        }
+    }
+}
